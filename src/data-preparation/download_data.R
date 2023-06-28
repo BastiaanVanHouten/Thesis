15844893f8a50a34fa46ae942c73191a033788e3
BastiaanVanHouten <b.a.vanhouten@tilburguniversity.edu>
2023-06-28 12:53:24 +0200

    added src to filter first dataset

--- src/data-preparation/download_data.R
+++ src/data-preparation/download_data.R
@@ -10,7 +10,7 @@ options(timeout = max(1000, getOption("timeout")))
 urls <- c("https://datasets.imdbws.com/title.basics.tsv.gz")
 
 ## Names for each URL
-names <- c("basics")
+names <- c("basics", "language")
 
 for (i in 1:length(urls)) {
     download.file(urls[i], paste0('data/', names[i], ".csv.gz"), mode = "wb")
--- src/data-preparation/view.dataset.R
+++ src/data-preparation/view.dataset.R
@@ -1,4 +1,24 @@
 library(tidyverse)
 library(dplyr)
 
-dir.create("../../gen/data-preparation/temp/", recursive = TRUE)
+dir.create("gen/data-preparation/temp/", recursive = TRUE)
+
+data <- read.delim("data/basics.csv.gz", header = FALSE, stringsAsFactors = FALSE)
+
+# Extract the first row as column names
+column_names <- as.character(data[1, ])
+data <- data[-1, ]  # Remove the first row
+
+# Set the extracted names as column names
+colnames(data) <- column_names
+
+# only movies > 2000
+data <- filter(data, titleType == "movie", startYear >= 2000, primaryTitle == originalTitle)
+
+# select only title , tconst, startYear, runtimeminutes, genres
+data <- select(data, tconst, primaryTitle, startYear, genres, runtimeMinutes)
+
+
+# save dataset
+write_csv(data, paste0('gen/data-preparation/temp/', "basics.csv"))
+
