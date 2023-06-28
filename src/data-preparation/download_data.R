97922d56aec3f95e08851576f5fa5cb27721a49e
BastiaanVanHouten <b.a.vanhouten@tilburguniversity.edu>
2023-06-28 11:49:11 +0200

    added the install package src and the src to download first file

---
+++ src/data-preparation/download_data.R
@@ -0,0 +1,17 @@
+## Load libraries
+library(tidyverse)
+library(dplyr)
+
+dir.create("data")
+
+## Load the URLs
+options(timeout = max(1000, getOption("timeout")))
+
+urls <- c("https://datasets.imdbws.com/title.basics.tsv.gz")
+
+## Names for each URL
+names <- c("basics")
+
+for (i in 1:length(urls)) {
+    download.file(urls[i], paste0('data/', names[i], ".csv.gz"), mode = "wb")
+}
