11431caa4b59b6a0c055af0dbb6c1d8b29c87d1f
BastiaanVanHouten <b.a.vanhouten@tilburguniversity.edu>
2023-07-15 20:07:29 +0200

    changes in the 10 cimmits

--- src/data-preparation/download_data.R
+++ src/data-preparation/download_data.R
@@ -1,17 +1,27 @@
 ## Load libraries
 library(tidyverse)
-library(dplyr)
 
 dir.create("data")
 
 ## Load the URLs
 options(timeout = max(1000, getOption("timeout")))
 
-urls <- c("https://datasets.imdbws.com/title.basics.tsv.gz")
+urls <- c("https://datasets.imdbws.com/title.basics.tsv.gz", 
+          "https://datasets.imdbws.com/title.crew.tsv.gz",
+          "https://datasets.imdbws.com/title.principals.tsv.gz",
+          "https://datasets.imdbws.com/name.basics.tsv.gz")
 
 ## Names for each URL
-names <- c("basics", "language")
+names <- c("basics", "director", "principals", "name.basics")
 
 for (i in 1:length(urls)) {
     download.file(urls[i], paste0('data/', names[i], ".csv.gz"), mode = "wb")
 }
+
+# Download zip from kaggle #
+url <- "https://www.kaggle.com/datasets/rounakbanik/the-movies-dataset/download?datasetVersionNumber=7"
+file_name <- "archive.zip"
+
+# Download the zip file
+download.file(url, destfile = file_name, mode = "wb")
+
